0caffa8b7ee6f433bae405659254d43e51df0e33
Jason A. Donenfeld <Jason@zx2c4.com>
2022-06-23 15:17:14 +0200

    ANDROID: cpu/hotplug: avoid breaking Android ABI by fusing cpuhp steps
    We can't add more values to the cpuhp_state enum, lest we break some
    vendor module. So instead break out existing steps into helper functions
    which call both steps. Fortunately none of these steps return anything
    other than 0, so we don't need to worry about rollback.
    
    Bug: 161946584
    Fixes: 5064550d422d ("random: clear fast pool, crng, and batches in cpuhp bring up")
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: Icaa61291207d799bab741be7bab2b636cda09422
    (cherry picked from commit 2c26b627277ef5ac79cfe10cf8183373ddddc4da)
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1390,6 +1390,20 @@ core_initcall(cpu_hotplug_pm_sync_init);
 
 int __boot_cpu_id;
 
+/* Horrific hacks because we can't add more to cpuhp_hp_states. */
+static int random_and_perf_prepare_fusion(unsigned int cpu)
+{
+	perf_event_init_cpu(cpu);
+	random_prepare_cpu(cpu);
+	return 0;
+}
+static int random_and_workqueue_online_fusion(unsigned int cpu)
+{
+	workqueue_online_cpu(cpu);
+	random_online_cpu(cpu);
+	return 0;
+}
+
 #endif /* CONFIG_SMP */
 
 /* Boot processor state steps */
@@ -1408,7 +1422,7 @@ static struct cpuhp_step cpuhp_hp_states[] = {
 	},
 	[CPUHP_PERF_PREPARE] = {
 		.name			= "perf:prepare",
-		.startup.single		= perf_event_init_cpu,
+		.startup.single		= random_and_perf_prepare_fusion,
 		.teardown.single	= perf_event_exit_cpu,
 	},
 	[CPUHP_WORKQUEUE_PREP] = {
@@ -1524,7 +1538,7 @@ static struct cpuhp_step cpuhp_hp_states[] = {
 	},
 	[CPUHP_AP_WORKQUEUE_ONLINE] = {
 		.name			= "workqueue:online",
-		.startup.single		= workqueue_online_cpu,
+		.startup.single		= random_and_workqueue_online_fusion,
 		.teardown.single	= workqueue_offline_cpu,
 	},
 	[CPUHP_AP_RCUTREE_ONLINE] = {
